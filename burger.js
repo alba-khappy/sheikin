6dcb44f84fd28c0de3a18b157c777d375e7caaaa
albinabist1993@mail.ru <b.hapilin@gateline.ru>
2023-05-04 14:31:41 +0300

    верстка всего лендинга с адаптивом

--- burger.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".burger-menu").addEventListener("click", function() {
-    document.querySelector(".nav__list").classList.add("nav__list--show"); 
-  });
-
-document.querySelector(".button-close").addEventListener("click", function() {
-    document.querySelector(".nav__list").classList.remove("nav__list--show"); 
-  });
